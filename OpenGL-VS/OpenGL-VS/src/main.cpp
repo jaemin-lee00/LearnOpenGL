291ee424bbe7a094dcf6ece7b2c31f9e71c560bd
jaemin lee <jemin0994@gmail.com>
2025-01-28 23:01:57 +0900

    ADD GITIGNORE

--- OpenGL-VS/OpenGL-VS/src/main.cpp
+++ OpenGL-VS/OpenGL-VS/src/main.cpp
@@ -54,7 +54,7 @@ const char *fragmentShaderSource = R"(
     uniform sampler2D ourTexture;
   
     void main() {
-        FragColor = texture(ourTexture, TexCoord);
+        FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);
     }
 )";
 
