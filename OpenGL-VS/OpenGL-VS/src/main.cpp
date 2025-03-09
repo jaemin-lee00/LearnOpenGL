86b2ab7b101e180d8774b4997952ba00dedb1589
jaemin lee <jemin0994@gmail.com>
2025-03-09 22:27:55 +0900

    Daily Commit 03/09

--- OpenGL-VS/OpenGL-VS/src/main.cpp
+++ OpenGL-VS/OpenGL-VS/src/main.cpp
@@ -12,7 +12,7 @@
 
 //Doucment adress
 //
-//  Last file update date : 2025-03-08 23:55
+//  Last file update date : 2025-03-09 22:00
 //
 //  now number, <<theme>> : 10 , Transformations
 //  https://heinleinsgame.tistory.com/   -number- , -Theme-
