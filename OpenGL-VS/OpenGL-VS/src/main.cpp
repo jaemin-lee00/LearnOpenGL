0db3ef389160a10a6e7303ab56702238fd9774d6
LEE JAEMIN <jemin0994@gmail.com>
2025-10-17 22:38:26 +0900

    feat: Add specular lighting to basic_lighting shader

--- OpenGL-VS/OpenGL-VS/src/main.cpp
+++ OpenGL-VS/OpenGL-VS/src/main.cpp
@@ -485,6 +485,8 @@ void mainLoop() {
         lightingShader->setVec3("objectColor", 1.0f, 0.5f, 0.31f);
         lightingShader->setVec3("lightColor", 1.0f, 1.0f, 1.0f);
 		lightingShader->setVec3("lightPos", lightPos);
+		lightingShader->setVec3("viewPos", camera.Position);
+
 
         setProjection(lightingShader);
         setCameraTransform(lightingShader);
