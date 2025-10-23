cfe6bee9f350a7d393e2e7d11f89fc61b6cf9b7b
LEE JAEMIN <jemin0994@gmail.com>
2025-10-23 22:34:32 +0900

    Adds basic lighting implementation
    Implements basic lighting calculations using ambient, diffuse, and specular components.
    
    Introduces a Light struct to encapsulate light properties such as position, ambient, diffuse, and specular colors.
    
    Sets initial light and ambient colors for testing purposes.

--- OpenGL-VS/OpenGL-VS/src/shaders/basic_lighting.fs
+++ OpenGL-VS/OpenGL-VS/src/shaders/basic_lighting.fs
@@ -7,6 +7,14 @@ struct Material {
 	float shininess;
 };
 
+struct Light {
+	vec3 position;
+	
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
 out vec4 FragColor;
 
 in vec3 FragPos;
@@ -22,19 +30,19 @@ void main()
 {
 	
 	// ambient
-	vec3 ambient = lightColor * material.ambient;
+	vec3 ambient = vec3(0.1) * material.ambient;
 
 	// Diffuse
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(lightPos - FragPos);
 	float diff = max(dot(norm, lightDir), 0.0);
-	vec3 diffuse = lightColor * (diff * material.diffuse);
+	vec3 diffuse = vec3(1.0) * (diff * material.diffuse);
 
 	// specular
 	vec3 viewDir = normalize(viewPos - FragPos);
 	vec3 reflectDir = reflect(-lightDir, norm);
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-	vec3 specular = lightColor * (spec * material.specular);
+	vec3 specular = vec3(1.0) * (spec * material.specular);
 
 	vec3 result = ambient + diffuse + specular;
 	FragColor = vec4(result, 1.0f);
