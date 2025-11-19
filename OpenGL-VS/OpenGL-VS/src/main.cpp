0f00c96f37d4fc5d1d1fd76f4fa753821cad40a3
LEE JAEMIN <jemin0994@gmail.com>
2025-11-19 14:48:02 +0900

    Implements a spotlight effect
    Implements a spotlight effect by using the camera's position and direction to define the light source.
    
    Also adjusts light parameters for a better visual appearance.

--- OpenGL-VS/OpenGL-VS/src/main.cpp
+++ OpenGL-VS/OpenGL-VS/src/main.cpp
@@ -471,12 +471,14 @@ void mainLoop() {
 
 		// Draw the cube
         lightingShader->use();
-		lightingShader->setVec3("light.direction", -0.2f, -1.0f, 0.3f);
+		lightingShader->setVec3("light.position", camera.Position);
+		lightingShader->setVec3("light.direction", camera.Front);
+		lightingShader->setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
 		lightingShader->setVec3("viewPos", camera.Position);
 
 		
-        lightingShader->setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-        lightingShader->setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+        lightingShader->setVec3("light.ambient", 0.1f, 0.1f, 0.1f);
+        lightingShader->setVec3("light.diffuse", 0.8f, 0.8f, 0.8f);
         lightingShader->setVec3("light.specular", 1.0f, 1.0f, 1.0f);
 		lightingShader->setFloat("light.constant", 1.0f);
 		lightingShader->setFloat("light.linear", 0.09f);
